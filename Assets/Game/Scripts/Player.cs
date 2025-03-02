845b3958e7d7fc93ce8f57c7edf836c58d1ab192
Speckles <speckleszzzz@gmail.com>
2025-03-02 12:05:11 +0530

    Testing 1
    MlAgent for movement is set
    1. Hit wall = -1 Reward
    2. Hit fireBall = -1 Reward
    3. Hit Ai = +1 Reward

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,6 +1,10 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class Player : MonoBehaviour
+public class Player : Agent
 {
     [Header("Player")]
     [SerializeField] float moveSpeed = 5f;
@@ -8,12 +12,61 @@ public class Player : MonoBehaviour
     [SerializeField] float xAxis;
     [SerializeField] float zAxis;
 
-    void Update()
-    {
-        xAxis = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        zAxis = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+    [Header("Enemy Data")]
+    [SerializeField] private Transform enemyTransform;
 
-        transform.Translate(xAxis,0f,zAxis);
+    public override void OnEpisodeBegin()
+    {
+        transform.position = new Vector3(-20f, 1f, 0f);
     }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(enemyTransform.position);
+    }
+
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        transform.position += new Vector3(moveX, 0f, moveZ) * moveSpeed * Time.deltaTime;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
+        continousActions[0] = Input.GetAxis("Horizontal");
+        continousActions[1] = Input.GetAxis("Vertical");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<EnemyShooter>(out EnemyShooter enemyShooter))
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
+
+        if (other.TryGetComponent<FireBall>(out FireBall fireBall))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+
+        if(other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+    // void Update()
+    // {
+    //     xAxis = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+    //     zAxis = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+    //     transform.Translate(xAxis,0f,zAxis);
+    // }
 
 }
